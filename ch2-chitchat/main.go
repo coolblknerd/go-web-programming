7d8f7e2a570306509d17c7b8031eb1d06faa7087
Reggie Davis <reginald.davis88@gmail.com>
2019-07-18 17:30:47 -0500

    Reorg'd the directory and added directory for ch 2

---
+++ ch2-chitchat/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
